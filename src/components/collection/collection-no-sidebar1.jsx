2a7a427008eb35926c3dbd62cf3ad973216c09ad
suzy <suzy14211@gmail.com>
2019-09-02 12:13:54 +0900

    0902 ckk merge전

--- src/components/collection/collection-no-sidebar1.jsx
+++ src/components/collection/collection-no-sidebar1.jsx
@@ -79,8 +79,12 @@ class CollectionCategory extends Component {
 
         console.log('id : ', id)
         const { items } = this.props;
+        const { item } = this.props;
+        const { category } = this.props;
 
         return (
+
+            
             <div>
                 <Breadcrumb title={'Collection'} />
 
@@ -98,7 +102,9 @@ class CollectionCategory extends Component {
                                                         <a href="#"><img src={`${process.env.PUBLIC_URL}/assets/images/mega-menu/2.jpg`}
                                                                          className="img-fluid" alt=""/></a>
                                                         <div className="top-banner-content small-section">
-                                                            <h4>fashion</h4>
+                                                            <h4>fashion 어디야
+                                                            {item.productName}
+                                                            </h4>
                                                             <h5>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</h5>
                                                             <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. </p>
                                                         </div>
@@ -168,7 +174,12 @@ class CollectionCategory extends Component {
 // }
 
 
-
+const mapStateToProps = state => ({
+   
+    filters: state.filters,
+    item : state.data.item,
+    category: state.data.category
+})
 
 
 const mapDispatchToProps = (dispatch) => ({
@@ -177,4 +188,4 @@ const mapDispatchToProps = (dispatch) => ({
 
 })
 
-export default withRouter(connect(null,mapDispatchToProps)(CollectionCategory));
+export default withRouter(connect(mapStateToProps,mapDispatchToProps)(CollectionCategory));
