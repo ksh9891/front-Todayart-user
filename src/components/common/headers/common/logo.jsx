a36001330c306c5fddb94998ba0f2321aef07882
suzy <suzy14211@gmail.com>
2019-09-02 11:36:29 +0900

    0902 logo수정 후 feature/merge merge전

--- src/components/common/headers/common/logo.jsx
+++ src/components/common/headers/common/logo.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import {Link} from 'react-router-dom'
+import Logo from '../common/TodayArtlogo10.png'
+
 
 function LogoImage(props) {
 
     return <Link to={`${process.env.PUBLIC_URL}/`} >
-                <img src={`${process.env.PUBLIC_URL}/assets/images/icon/${props.logo}`} alt="" className="img-fluid" />
+                <img src={Logo} alt="" className="img-fluid" />
             </Link>;
 }
 
--- src/components/common/related-product.jsx
+++ src/components/common/related-product.jsx
@@ -7,8 +7,15 @@ import {getBestSeller} from "../../services";
 import {addToCart, addToWishlist, addToCompare} from "../../actions";
 import ProductItem from '../layouts/common/product-item';
 
+import {Actions} from '../../actions'
 
 class RelatedProduct extends Component {
+
+    componentWillMount(){
+        // this.fetchMoreItems();
+        this.props.fetchArtwork();
+    }
+
     render (){
         const {items, symbol, addToCart, addToWishlist, addToCompare} = this.props;
 
@@ -18,10 +25,24 @@ class RelatedProduct extends Component {
                 <div className="container">
                     <div className="row">
                         <div className="col-12 product-related">
-                            <h2>related products</h2>
+                            <h2>related products 추천 상품</h2>
                         </div>
                     </div>
-                    <div className="row search-product">
+
+
+                                <div className="no-slider row">
+                                    { items.slice(0, 4).map((item, index ) =>
+                                        <ProductItem item={item} symbol={symbol}
+                                                     onAddToCompareClicked={() => addToCompare(item)}
+                                                     onAddToWishlistClicked={() => addToWishlist(item)}
+                                                     onAddToCartClicked={() => addToCart(item, 1)} key={index} /> )
+                                    }
+                                </div>
+                      
+
+
+
+                    {/* <div className="row search-product">
                         { items.slice(0, 6).map((item, index ) =>
                             <div key={index} className="col-xl-2 col-md-4 col-sm-6">
                                 <ProductItem item={item} symbol={symbol}
@@ -30,18 +51,32 @@ class RelatedProduct extends Component {
                                              onAddToCartClicked={() => addToCart(item, 1)} key={index} />
                             </div>)
                         }
-                    </div>
+                    </div> */}
                 </div>
             </section>
         )
     }
 }
 
-function mapStateToProps(state) {
-    return {
-        items: getBestSeller(state.data.products),
-        symbol: state.data.symbol
-    }
-}
+// function mapStateToProps(state) {
+//     return {
+//         items: getBestSeller(state.data.products),
+//         symbol: state.data.symbol,
+        
+//     }
+// }
 
-export default connect(mapStateToProps, {addToCart, addToWishlist, addToCompare})(RelatedProduct);
+// export default connect(mapStateToProps, {addToCart, addToWishlist, addToCompare})(RelatedProduct);
+
+const mapStateToProps = (state) => ({ 
+    symbol: state.data.symbol,
+    items: state.data.items
+})
+
+const mapDispatchToProps = (dispatch) => ({
+    // fetchCategory: (id) => dispatch(Actions.fetchCategory(id)),
+    fetchArtwork:() => dispatch(Actions.fetchArtwork()) 
+   
+})
+
+export default connect(mapStateToProps, mapDispatchToProps) (RelatedProduct);
